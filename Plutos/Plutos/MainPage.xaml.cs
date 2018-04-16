de027c1e801c82bc4409bb9d3b6f0aa5d9435e63
IRechar <immanuel.rechar@outlook.com>
2018-04-16 09:01:04 +0200

    Finished sidebar

---
+++ Plutos/Plutos/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ViewManagement;
+using Windows.Graphics.Display;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+namespace Plutos
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MaximizedWindow();
+        }
+
+        bool sidebarmaxed = false;
+
+        private void MaximizedWindow()
+        {
+            var view = DisplayInformation.GetForCurrentView();
+
+            // Get the screen resolution (APIs available from 14393 onward).
+            var resolution = new Size(view.ScreenWidthInRawPixels, view.ScreenHeightInRawPixels);
+
+            // Calculate the screen size in effective pixels. 
+            // Note the height of the Windows Taskbar is ignored here since the app will only be given the maxium available size.
+            var scale = view.ResolutionScale == ResolutionScale.Invalid ? 1 : view.RawPixelsPerViewPixel;
+            var bounds = new Size(resolution.Width / scale, resolution.Height / scale);
+
+            ApplicationView.PreferredLaunchViewSize = new Size(bounds.Width, bounds.Height);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        private void ChangeMode(object sender, RoutedEventArgs e)
+        {
+            if (sidebarmaxed)
+            {
+                Sidebar.Width = 70;
+                sidebarmaxed = false;
+                ViewButton.HorizontalAlignment = HorizontalAlignment.Stretch;
+            }
+            else
+            {
+                Sidebar.Width = 200;
+                sidebarmaxed = true;
+                ViewButton.HorizontalAlignment = HorizontalAlignment.Right;
+            }
+        }
+    }
+}
